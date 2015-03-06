7f4da7308856db475cb732d070ca2f0d445d2340
girinath <girinath07@gmail.com>
2015-03-06 15:44:55 +0530

    roll back change

--- src/scripts/SR6.java
+++ src/scripts/SR6.java
@@ -21,7 +21,7 @@ public class SR6 {
 		wm.clickValueByLinkText("My Account");
 		wm.setValueById("email", email);
 		wm.setValueById("pass", "nathgg");
-		wm.clickById("send2sujintest");
+		wm.clickById("send2");
 
 		wm.clickValueByXpathAttr("div", "class", "\"block-content\"",
 				"/ul/li[8]/a");
